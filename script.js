618fd3e3ca799062bcd725b7c97d7d23ff8a3147
L1nar <122251664+L1nar@users.noreply.github.com>
2023-12-10 17:09:37 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
-let input = document.querySelectorAll('#number');
-let btn = document.querySelectorAll('#btn');
+let number1 = document.querySelector('#number1');
+let number2 = document.querySelector('#number2');
+let signs = document.querySelector('#math-signs');
+let equals = document.querySelector('#btn');
 let output = document.querySelector('#answer');
 
-let num1 = 0;
-let num2 = 0;
-btn.addEventListener('click', function () {
-    num1 = Number(input.value);
+function sum(num1, num2) {
+    return num1 + num2;
+}
+function dif(num1, num2) {
+    return num1 - num2;
+}
+function product(num1, num2) {
+    return num1 * num2;
+}
+function quotient(num1, num2) {
+    return num1 / num2;
+}
+
+equals.addEventListener('click', function () {
+    let num1 = Number(number1.value);
+    let num2 = Number(number2.value);
+    let operation = signs.value;
     console.log(num1);
-});
-btn.addEventListener('click', function () {
-    num2 = Number(input.value);
     console.log(num2);
+    console.log(operation);
+    switch (operation) {
+        case '+': output.value = sum(num1, num2);
+            break;
+    }
+    switch (operation) {
+        case '-': output.value = dif(num1, num2);
+            break;
+    }
+    switch (operation) {
+        case '*': output.value = product(num1, num2);
+            break;
+    }
+    switch (operation) {
+        case '/': output.value = quotient(num1, num2);
+            break;
+    }
 });
